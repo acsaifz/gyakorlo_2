61423f1310d4d8cd2f4a5e0df2660267c8945e4b
AcsaiFZ <acsai.fz@gmail.com>
2022-03-13 13:17:42 +0100

    Initial commit

--- src/hu/progmatic/medve_csata.java
+++ src/hu/progmatic/medve_csata.java
@@ -1,8 +1,23 @@
 package hu.progmatic;
 
 public class medve_csata {
-
     public static void main(String[] args) {
-	// write your code here
+	    int hp1 = 150;
+        int ap1 = 85;
+        int hp2 = 170;
+        int ap2 = 75;
+
+        do{
+            hp1 -= ap2;
+            hp2 -= ap1;
+            System.out.println("Első Medve: " + hp1 + " HP \t" +
+                    "           Második Medve: " + hp2 + " HP");
+        }while(hp1 > 0 && hp2 > 0);
+
+        if (hp1 == hp2){
+            System.out.println("Döntetlen");
+        }else {
+            System.out.println((hp1 > hp2 ? "Első " : "Második") + " medve nyert!");
+        }
     }
 }
